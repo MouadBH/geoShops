38b607e7e452b74c325594c2f4259ac5ed0934ef
MouadBH <mouad123b@gmail.com>
2019-05-17 17:27:22 +0000

    bismilah tawakalto 3ala lah

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css">
+
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+
+
+            <div class="content">
+                <div id="example"></div>
+
+            </div>
+        </div>
+
+        <script src="{{ asset('js/app.js') }}" ></script>
+    </body>
+</html>
